84c52725b93f177ce9b42f12c94b139f2224b9fa
Shelby Hominger <stoveship@gmail.com>
2026-07-11 20:20:33 -0400

    fix: inject shadcn theme extensions into tailwind config

---
+++ apps/web/.next/server/middleware-build-manifest.js
@@ -0,0 +1,21 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/0cz1d0mv5g_q7.js"
+  ],
+  "lowPriorityFiles": [
+    "static/bh-_5BmyWIDRZm065_8ST/_buildManifest.js",
+    "static/bh-_5BmyWIDRZm065_8ST/_ssgManifest.js",
+    "static/bh-_5BmyWIDRZm065_8ST/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/3v0wi6a9lijkh.js",
+    "static/chunks/0_mkxdzhg5j1a.js",
+    "static/chunks/1ipcf9xp-4fwc.js",
+    "static/chunks/0qij6m43va1on.js",
+    "static/chunks/turbopack-0uiy87xgget8r.js"
+  ]
+};
---
+++ apps/web/.next/static/bh-_5BmyWIDRZm065_8ST/_clientMiddlewareManifest.js
@@ -0,0 +1,10 @@
+self.__MIDDLEWARE_MATCHERS = [
+  {
+    "regexp": "^(?:\\/(_next\\/data\\/[^/]{1,}))?\\/api(?:\\/((?:[^\\/#\\?]+?)(?:\\/(?:[^\\/#\\?]+?))*))?(\\.json|\\.rsc|\\.segments\\/.+\\.segment\\.rsc)?[\\/#\\?]?$",
+    "originalSource": "/api/:path*"
+  },
+  {
+    "regexp": "^(?:\\/(_next\\/data\\/[^/]{1,}))?(?:\\/((?!_next\\/static|_next\\/image|favicon.ico).*))(\\.json|\\.rsc|\\.segments\\/.+\\.segment\\.rsc)?[\\/#\\?]?$",
+    "originalSource": "/((?!_next/static|_next/image|favicon.ico).*)"
+  }
+];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
